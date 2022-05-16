e801fb98f7faf3b5f7311279c76a862801a9d7e6
costaaaaa <costamagna551@gmail.com>
2022-05-16 18:34:41 +0200

    aggiunte icone alla pagina della azioni

--- portafoglio/azione.php
+++ portafoglio/azione.php
@@ -51,14 +51,15 @@
                             Azioni
                         </a>
                     </li>
-                    <?php session_start(); if(isset($_SESSION['username'])){
-                            echo '<li class="nav-item mx-0 mx-lg-1" role="presentation" id="linkAccount">
+                    <?php session_start();
+                    if (isset($_SESSION['username'])) {
+                        echo '<li class="nav-item mx-0 mx-lg-1" role="presentation" id="linkAccount">
                             <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="./account.php">
                                 Account
                             </a>
                         </li>';
-                        }else{
-                            
+                    } else {
+
                         echo '<li class="nav-item mx-0 mx-lg-1" role="presentation">
                             <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="./login.html">
                                 Log-in
@@ -69,7 +70,7 @@
                                 Sign-up
                             </a>
                         </li>';
-                        } ?>
+                    } ?>
                     </li>
 
                     <!-- 'tour' della pagina-->
@@ -108,7 +109,12 @@
                     <?php
                     $nome = $_POST['nome'];
                     $simbolo = $_POST['simbolo'];
-                    echo '<input id="btn-grafico" type="button" class="btn btn-primary" onclick="grafico(`' . $simbolo . '`)" value="Aggiorna grafico ' . $nome . '" />';
+                    echo '<button id="btn-grafico" type="button" class="btn btn-primary" onclick="grafico(`' . $simbolo . '`)">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-graph-up" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M0 0h1v15h15v1H0V0Zm14.817 3.113a.5.5 0 0 1 .07.704l-4.5 5.5a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61 4.15-5.073a.5.5 0 0 1 .704-.07Z"/>
+                            </svg>
+                            &nbsp
+                            Aggiorna grafico ' . $nome . '</button>';
                     ?>
                 </div>
                 <div class="col">
@@ -120,6 +126,13 @@
                         <input type="number" id="numAzioni" name="numAzioni" placeholder="Numero di azioni" required>
                         <br /><br />
                         <button id="btn-acquista-azione" class="btn btn-primary" type="submit">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cash-coin" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0z" />
+                                <path d="M9.438 11.944c.047.596.518 1.06 1.363 1.116v.44h.375v-.443c.875-.061 1.386-.529 1.386-1.207 0-.618-.39-.936-1.09-1.1l-.296-.07v-1.2c.376.043.614.248.671.532h.658c-.047-.575-.54-1.024-1.329-1.073V8.5h-.375v.45c-.747.073-1.255.522-1.255 1.158 0 .562.378.92 1.007 1.066l.248.061v1.272c-.384-.058-.639-.27-.696-.563h-.668zm1.36-1.354c-.369-.085-.569-.26-.569-.522 0-.294.216-.514.572-.578v1.1h-.003zm.432.746c.449.104.655.272.655.569 0 .339-.257.571-.709.614v-1.195l.054.012z" />
+                                <path d="M1 0a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.083c.058-.344.145-.678.258-1H3a2 2 0 0 0-2-2V3a2 2 0 0 0 2-2h10a2 2 0 0 0 2 2v3.528c.38.34.717.728 1 1.154V1a1 1 0 0 0-1-1H1z" />
+                                <path d="M9.998 5.083 10 5a2 2 0 1 0-3.132 1.65 5.982 5.982 0 0 1 3.13-1.567z" />
+                            </svg>
+                            &nbsp
                             Acquista azioni
                         </button>
                     </form>
